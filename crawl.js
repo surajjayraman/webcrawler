38d871b05b81b81e5073c9f681e2da99c4ba0403
surajjayraman <suraj.jayraman@gmail.com>
2022-12-25 15:03:53 -0500

    Add new function getURLsFromHTML.

--- crawl.js
+++ crawl.js
@@ -1,3 +1,28 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const aElements = dom.window.document.querySelectorAll('a');
+  for (const aElement of aElements) {
+    if (aElement.href.slice(0,1) === '/') {
+      try {
+        urls.push(new URL(aElement.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    }
+  }
+  console.log(urls);
+  return urls;
+}
+
 function normalizeURL(url) {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.host}${urlObj.pathname}`;
@@ -8,5 +33,6 @@ function normalizeURL(url) {
 }
   
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js');
+const { normalizeURL,getURLsFromHTML } = require('./crawl.js');
 const { test, expect } = require('@jest/globals');
 
 test('normalizeURL protocol', () => {
@@ -26,5 +26,13 @@ test('normalizeURL http', () => {
   const input = 'http://BLOG.boot.dev/path';
   const actual = normalizeURL(input);
   const expected = 'blog.boot.dev/path';
+  expect(actual).toEqual(expected);
+});
+
+test('getURLsFromHTML absolute', () => {
+  const inputURL = 'https://blog.boot.dev';
+  const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [ 'https://blog.boot.dev/' ];
   expect(actual).toEqual(expected);
 });
