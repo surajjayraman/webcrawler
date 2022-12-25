69902517b7aa64b6fe9dce34ffe78636df2e3bd4
surajjayraman <suraj.jayraman@gmail.com>
2022-12-25 18:46:10 -0500

    Add function to fetch and parse the html of the current url.

--- crawl.js
+++ crawl.js
@@ -1,4 +1,25 @@
+const fetch = require("node-fetch");
 const { JSDOM } = require('jsdom');
+
+async function crawlPage(currentURL) {
+  // fetch and parse the html of the currentURL
+  console.log(`Analyzing dependencies ${currentURL}`);
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 399) {
+      console.log(`Got HTTP error, status code: ${resp.status}`);
+      return;
+    }
+    const contentType = resp.headers.get('content-type');
+    if (!contentType.includes('text/html')) {
+      console.log(`Got non-html response: ${contentType}`);
+      return;
+    }
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(err.message);
+  }
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
@@ -34,5 +55,6 @@ function normalizeURL(url) {
   
 module.exports = {
   normalizeURL,
-  getURLsFromHTML
+  getURLsFromHTML,
+  crawlPage
 };
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-function main() {
+const { crawlPage } = require('./crawl.js');
+
+async function main() {
   if (process.argv.length < 3) {
     console.log('no website provided');
+    return;
   }
   if (process.argv.length > 3) {
     console.log('too many arguments provided');
+    return;
   }
   
   const baseURL = process.argv[2];
   
   console.log(`starting JSDOM Link Analyzer of: ${baseURL}`);
+  await crawlPage(baseURL, baseURL, {});
 }
   
 main();
