5e4b2fdc32f590776e6cdbe295adb5b8ae136055
surajjayraman <suraj.jayraman@gmail.com>
2022-12-25 17:41:55 -0500

    Create a main function with cmd line inputs.

--- crawl.test.js
+++ crawl.test.js
@@ -30,9 +30,26 @@ test('normalizeURL http', () => {
 });
 
 test('getURLsFromHTML absolute', () => {
-  const inputURL = 'https://blog.boot.dev';
-  const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>';
+  const inputURL = 'https://www.points.com/';
+  const inputBody = '<html><body><a data-type="gatsby-link" href="/contact/schedule-appointment/">Get started</a>/body></html>';
   const actual = getURLsFromHTML(inputBody, inputURL);
-  const expected = [ 'https://blog.boot.dev/' ];
+  const expected = [ 'https://www.points.com/contact/schedule-appointment/' ];
   expect(actual).toEqual(expected);
 });
+
+test('getURLsFromHTML relative', () => {
+  const inputURL = 'https://blog.boot.dev';
+  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [ 'https://blog.boot.dev/path/one' ];
+  expect(actual).toEqual(expected);
+});
+  
+test('getURLsFromHTML both', () => {
+  const inputURL = 'https://blog.boot.dev';
+  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="/path/two"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [ 'https://blog.boot.dev/path/one', 'https://blog.boot.dev/path/two' ];
+  expect(actual).toEqual(expected);
+});
+
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+function main() {
+  if (process.argv.length < 3) {
+    console.log('no website provided');
+  }
+  if (process.argv.length > 3) {
+    console.log('too many arguments provided');
+  }
+  
+  const baseURL = process.argv[2];
+  
+  console.log(`starting JSDOM Link Analyzer of: ${baseURL}`);
+}
+  
+main();
